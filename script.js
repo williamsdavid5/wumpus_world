3527f760bb4006e5bcb4c0a7f094ae77e67807ff
David Duarte (Williams) <davidduart04@gmail.com>
2025-02-01 09:28:51 -0300

    modificacao na logica de insercao, adicao de fedor e brisa

--- script.js
+++ script.js
@@ -9,7 +9,9 @@ function mapaString(d) {
     //tags de cada elemento
     let pocoTag = "O";
     let outoTag = "$";
-    let wumpusTag = ":("
+    let wumpusTag = ":(";
+    let brisaTag = "p#";
+    let fedorTag = "w#";
 
     //gera uma matriz preenchida
     let matriz = Array.from({ length: d }, () => Array(d).fill("X"));
@@ -34,32 +36,54 @@ function mapaString(d) {
 
     //as posições resgatadas sao embaralhadas, assim os elementos serao inseridos de forma aleatoria
     posicoes.sort(() => Math.random() - 0.5);
-    console.log(posicoes)
 
     //essa matriz de posições é percorrida e os elementos sao inseridos baseado nela
+    // let index = 0;
+    // for (let { tag, quantidade } of elementos) {
+    //     for (let k = 0; k < quantidade && index < posicoes.length; k++) {
+    //         let [x, y] = posicoes[index++];
+
+    //         if (tag == pocoTag) {
+    //             if (matriz[x][y] === "X") {
+    //                 matriz[x][y] = tag;
+    //                 marcarPerigo(matriz, x, y, d, brisaTag)
+    //             }
+    //         } else {
+    //             if (matriz[x][y] != pocoTag && matriz[x][y] != outoTag && matriz[x][y] !== wumpusTag) {
+    //                 // matriz[x][y] = "";
+    //                 if (matriz[x][y] === "X") {
+    //                     matriz[x][y] = "";
+    //                 }
+    //                 matriz[x][y] += tag;
+    //             } else if (matriz[x][y] === "X") {
+    //                 matriz[x][y] = tag;
+    //             }
+    //         }
+    //     }
+    // }
+
     let index = 0;
     for (let { tag, quantidade } of elementos) {
         for (let k = 0; k < quantidade && index < posicoes.length; k++) {
             let [x, y] = posicoes[index++];
 
-            if (tag == pocoTag) {
-                if (matriz[x][y] === "X") {
-                    matriz[x][y] = tag;
-                }
+            if (tag === pocoTag) {
+                matriz[x][y] = tag;
+                marcarPerigo(matriz, x, y, d, brisaTag);
+            } else if (tag === wumpusTag) {
+                matriz[x][y] = tag;
+                marcarPerigo(matriz, x, y, d, fedorTag);
             } else {
-                if (matriz[x][y] != pocoTag && matriz[x][y] != outoTag && matriz[x][y] !== wumpusTag) {
-                    // matriz[x][y] = "";
-                    if (matriz[x][y] === "X") {
-                        matriz[x][y] = "";
-                    }
-                    matriz[x][y] += tag;
-                } else if (matriz[x][y] === "X") {
-                    matriz[x][y] = tag;
+                if (matriz[x][y] === "X") {
+                    matriz[x][y] = "";
                 }
+
+                matriz[x][y] += tag;
             }
         }
     }
 
+    console.log(matriz);
     mapaStyle(d, matriz);
 
     // document.getElementById("saida").innerHTML = matriz.map(linha => linha.join(" | ")).join("<br>");
@@ -70,27 +94,76 @@ function mapaString(d) {
     // document.getElementById("saidaDados").innerHTML += "<p> flecha: " + flecha + "</p>";
 }
 
+function marcarPerigo(matriz, x, y, d, perigotag) {
+    let direcoes = [
+        [-1, 0], [1, 0], [0, -1], [0, 1]
+    ];
+
+    for (let [dx, dy] of direcoes) {
+        let nx = x + dx, ny = y + dy;
+        if (nx >= 0 && nx < d && ny >= 0 && ny < d) {
+            if (matriz[nx][ny] == "X") {
+                matriz[nx][ny] = perigotag;
+            } else if (matriz[nx][ny] !== "O" && matriz[nx][ny] !== ":(") {
+                matriz[nx][ny] += perigotag;
+            }
+        }
+    }
+}
+
 function mapaStyle(d, matriz) {
+
+    mapaTamanho = 100;
+
     let mapa = document.getElementById("mapa");
-    mapa.style.width = d * 100 + 'px';
-    mapa.style.height = d * 100 + 'px';
+    mapa.style.width = d * mapaTamanho + 'px';
+    mapa.style.height = d * mapaTamanho + 'px';
     mapa.style.display = 'grid';
-    mapa.style.gridTemplateColumns = 'repeat(' + d + ', 100px)';
-    mapa.style.gridTemplateRows = 'repeat(' + d + ', 100px)';
+    mapa.style.gridTemplateColumns = 'repeat(' + d + ', ' + mapaTamanho + 'px)';
+    mapa.style.gridTemplateRows = 'repeat(' + d + ', ' + mapaTamanho + 'px)';
+
+    // for (let i = 0; i < d; i++) {
+    //     for (let j = 0; j < d; j++) {
+    //         if (matriz[i][j] === 'X') {
+    //             document.getElementById("mapa").innerHTML += "<div id=\"sala\"></div>"
+    //         }
+    //         if (matriz[i][j] === 'O') {
+    //             document.getElementById("mapa").innerHTML += "<div id=\"sala\"><div id=\"poco\"></div></div>"
+    //         }
+    //         if (matriz[i][j] === '$') {
+    //             document.getElementById("mapa").innerHTML += "<div id=\"sala\"><h1>$</h1></div>"
+    //         }
+    //         if (matriz[i][j] === ':(') {
+    //             document.getElementById("mapa").innerHTML += "<div id=\"sala\"><h1>:(</h1></div>"
+    //         }
+    //         if (matriz[i][j] === ':($' || matriz[i][j] === '$:(') {
+    //             document.getElementById("mapa").innerHTML += "<div id=\"sala\"><h1>:( $</h1></div>"
+    //         }
+    //     }
+    // }
 
     for (let i = 0; i < d; i++) {
         for (let j = 0; j < d; j++) {
-            if (matriz[i][j] === 'X') {
-                document.getElementById("mapa").innerHTML += "<div id=\"sala\"></div>"
+            document.getElementById("mapa").innerHTML += "<div id=" + i + "," + j + " class = \"sala\"></div>"
+        }
+    }
+
+    for (let i = 0; i < d; i++) {
+        for (let j = 0; j < d; j++) {
+            if (matriz[i][j].includes("O")) {
+                document.getElementById(i + "," + j).innerHTML += "<h1>O</h1>";
             }
-            if (matriz[i][j] === 'O') {
-                document.getElementById("mapa").innerHTML += "<div id=\"sala\"><div id=\"poco\"></div></div>"
+            if (matriz[i][j].includes(":(")) {
+                document.getElementById(i + "," + j).innerHTML += "<h1>:(</h1>";
             }
-            if (matriz[i][j] === '$') {
-                document.getElementById("mapa").innerHTML += "<div id=\"sala\"><h1>$</h1></div>"
+            if (matriz[i][j].includes("$")) {
+                document.getElementById(i + "," + j).innerHTML += "<h1>$</h1>";
             }
-            if (matriz[i][j] === ':(') {
-                document.getElementById("mapa").innerHTML += "<div id=\"sala\"><h1>:(</h1></div>"
+            if (matriz[i][j].includes("p#")) {
+                document.getElementById(i + "," + j).innerHTML += "<p>brisa</p>";
+            }
+            if (matriz[i][j].includes("w#")) {
+                document.getElementById(i + "," + j).innerHTML += "<p>fedor</p>";
             }
         }
     }
