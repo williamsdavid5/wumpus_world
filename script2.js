eb2f01e6bd0a63e563b7f79c31ec7fdb16b818cb
David Duarte (Williams) <davidduart04@gmail.com>
2025-02-06 09:29:30 -0300

    adicionado movimento aleatorio, mortes e vitoria ao pegar todos os ouros - falta disparar flecha

--- script2.js
+++ script2.js
@@ -7,6 +7,11 @@ class Sala {
         this.brisa = false;
         this.fedor = false;
         this.ouro = false;
+
+        this.norte = null;
+        this.sull = null;
+        this.leste = null;
+        this.oeste = null;
     }
 }
 
@@ -17,8 +22,58 @@ class Wumpus {
 }
 
 class Agente {
-    constructor(flechas) {
+    constructor(flechas, mundo) {
         this.flechas = flechas;
+        this.mundo = mundo;
+        this.ouro = 0;
+
+        this.x = 0;
+        this.y = 0;
+
+        this.sala = mundo[this.x][this.y];
+
+        this.mortes = 0;
+        this.vitorias = 0;
+    }
+
+    moverNorte() {
+        try {
+            this.sala = mundo[this.x - 1][this.y];
+            this.x -= 1;
+
+        } catch (e) {
+            console.log("erro ao mover para o norte");
+        }
+    }
+
+    moverSul() {
+        try {
+            this.sala = mundo[this.x + 1][this.y];
+            this.x += 1;
+
+        } catch (e) {
+            console.log("erro ao mover para o sul");
+        }
+    }
+
+    moverLeste() {
+        try {
+            this.sala = mundo[this.x][this.y + 1];
+            this.y += 1;
+
+        } catch (e) {
+            console.log("erro ao mover para o leste");
+        }
+    }
+
+    moverOeste() {
+        try {
+            this.sala = mundo[this.x][this.y - 1];
+            this.y -= 1;
+
+        } catch (e) {
+            console.log("erro ao mover para o oeste");
+        }
     }
 }
 
@@ -118,9 +173,92 @@ function renderizarMapa(mapaTamanho, d) {
     }
 }
 
-let mundo = [];
-let agente = new Agente(0);
+function inserirAgenteNoMundo() {
 
+    let novoAgente = new Agente(flecha, mundo);
+
+    document.getElementById(0 + "," + 0).innerHTML += "<img src=\"textures/agente.png\" id=\"agente\" alt=\"\">";
+    document.getElementById("mortesPontuacao").textContent = "Mortes: " + novoAgente.mortes;
+    document.getElementById("vitoriasPontuacao").textContent = "Vitorias: " + novoAgente.vitorias;
+    document.getElementById("flechasNumero").textContent = "Flechas: " + novoAgente.flechas;
+
+    return novoAgente;
+}
+
+function restaurarMundo(mundo, posicoesOuro) {
+    posicoesOuro.forEach(([x, y]) => {
+        mundo[x][y].ouro = true;
+        document.getElementById(x + "," + y).innerHTML += "<img src=\"textures/azedinha.png\" id=\"" + x + "," + y + "_ouroItem\" alt=\"\">";
+    });
+
+    posicoesOuro.length = 0;
+
+}
+
+function rodarGameAleatorio(agente, mundo, posicoesOuro, ouro, flecha) {
+    document.getElementById("agente").remove();
+
+    let movimentos = [];
+
+    if (mundo[agente.x - 1] && mundo[agente.x - 1][agente.y] !== undefined) {
+        movimentos.push(() => agente.moverNorte());
+    }
+    if (mundo[agente.x + 1] && mundo[agente.x + 1][agente.y] !== undefined) {
+        movimentos.push(() => agente.moverSul());
+    }
+    if (mundo[agente.x] && mundo[agente.x][agente.y + 1] !== undefined) {
+        movimentos.push(() => agente.moverLeste());
+    }
+    if (mundo[agente.x] && mundo[agente.x][agente.y - 1] !== undefined) {
+        movimentos.push(() => agente.moverOeste());
+    }
+
+    movimentos[Math.floor(Math.random() * movimentos.length)]();
+    console.log(agente.x, agente.y);
+
+    if (mundo[agente.x][agente.y].wumpus) {
+        console.log("wumpus aqui!");
+        agente.x = agente.y = 0;
+        agente.mortes += 1;
+        agente.ouro = 0;
+        agente.flecha = flecha;
+        document.getElementById("mortesPontuacao").textContent = "Mortes: " + agente.mortes;
+        restaurarMundo(mundo, posicoesOuro);
+    }
+
+    if (mundo[agente.x][agente.y].buraco) {
+        console.log("buraco aqui!");
+        agente.x = agente.y = 0;
+        agente.mortes += 1;
+        agente.ouro = 0;
+        agente.flecha = flecha;
+        document.getElementById("mortesPontuacao").textContent = "Mortes: " + agente.mortes;
+        restaurarMundo(mundo, posicoesOuro);
+    }
+
+    if (mundo[agente.x][agente.y].ouro) {
+        console.log("ouro aqui!");
+        posicoesOuro.push([agente.x, agente.y]);
+        mundo[agente.x][agente.y].ouro = false;
+        agente.ouro += 1;
+        document.getElementById(agente.x + "," + agente.y + "_ouroItem").remove();
+    }
+
+    if (agente.x == 0 && agente.y == 0 && agente.ouro == ouro) {
+        console.log("venceu!");
+        agente.vitorias += 1;
+        agente.ouro = 0;
+        agente.flecha = flecha;
+        document.getElementById("vitoriasPontuacao").textContent = "Vitorias: " + agente.vitorias;
+        restaurarMundo(mundo, posicoesOuro);
+    }
+
+    document.getElementById(agente.x + "," + agente.y).innerHTML += "<img src=\"textures/agente.png\" id=\"agente\" alt=\"\">";
+
+}
+
+
+let mundo = [];
 let d = 4;
 
 let buraco = Math.floor(Math.random() * (d - 1)) + 1;
@@ -128,7 +266,17 @@ let ouro = Math.floor(Math.random() * (d - buraco - 1)) + 1;
 let wumpus = ouro;
 let flecha = wumpus;
 
+let posicoesOuro = [];
+
 criarMundo(d, mundo);
 adicionaEntidades(mundo, d, wumpus, buraco, ouro);
 
 renderizarMapa(100, d);
+
+let agente = inserirAgenteNoMundo(mundo, flecha);
+
+// rodarGameAleatorio(agente);
+
+setInterval(() => {
+    rodarGameAleatorio(agente, mundo, posicoesOuro, ouro);
+}, 500);
